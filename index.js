c6f1985309d358d52144f08cd55dda3782e9bfb3
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-03-21 20:37:10 +0300

    Completed  assignment

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = ['cow', 'horse', 'sheep', 'pig', 'chicken'];
+const [moo, neigh, baa, oink, cluck] = farmAnimals
+console.log(moo, neigh, baa, oink, cluck);
+
+const traditionalAnimals = ['cow', 'sheep','pig','chicken'];
+const [bessie, dolly, babe, little] =traditionalAnimals
+console.log(bessie, dolly, babe, little);
+
+const traditionalAnimalsColors = ['cow', 'sheep','pig'];
+const [blackAndWhite, black, pink]= traditionalAnimalsColors
+console.log(blackAndWhite, black, pink);
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet]= colors
+console.log(red, orange, yellow, green, blue, indigo, violet);
 
-const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
-};
+const colorInit = ['red', 'orange', 'yellow', 'green', 'blue', 'violet'] 
+const [r, o, y, g, b, v] = colorInit
+console.log(r, o, y, g, b, v);
 
-const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
-};
+const color7 = ['indigo']
+const [indg] = color7;
+console.log(indg);
+
+
+
+const muppetName = 'Miss Piggy'
+  const color =  'pink'
+ const song = 'Never Before, Never Again'
+ const job = 'Cast member of The Muppet Show'
+ const partner = 'Kermit'
+
+//const nestedMuppet = {
+ // nestedName: 'Kermit',
+ // nestedColor: 'green',
+  //album: {
+   // theMuppetMovie: {
+     // song1: 'Rainbow Connection',
+     const song2 = 'Moving Right Along'
+     // song3: 'Never Before, Never Again',
+     const song4 = 'I Hope That Something Better Comes Along'
+   // },
+ // },
+  const nestedJob = 'Host of The Muppet Show'
+ const nestedPartner = 'Miss Piggy'
+//};
 
 // Strings
 
